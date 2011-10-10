1053e26208c70cdecf1eb2962e57c4673713b141
davk789 <david.joseph.kendall@gmail.com>
2011-10-09 22:57:04 -0700

    added XCode template

---
+++ fOSC/Classes/fOSCViewController.h
@@ -0,0 +1,16 @@
+//
+//  fOSCViewController.h
+//  fOSC
+//
+//  Created by David Kendall on 10/9/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fOSCViewController : UIViewController {
+
+}
+
+@end
+
