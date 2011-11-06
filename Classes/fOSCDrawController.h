987b2a07e2e3b6b64238dc879330e646c20b74cb
davk789 <david.joseph.kendall@gmail.com>
2011-11-06 09:17:56 -0800

    added draw controller

---
+++ Classes/fOSCDrawController.h
@@ -0,0 +1,13 @@
+//
+//  fOSCDrawController.h
+//  fOSC
+//
+//  Created by David Kendall on 11/6/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface fOSCDrawController : UIViewController
+
+@end
